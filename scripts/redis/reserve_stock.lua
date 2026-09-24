696683b0fb550db82e5bf70f38be8af9a58acfa8
AndroDeMohawk <joker110sem777@gmail.com>
2026-09-24 12:39:10 +0300

    commit

--- scripts/redis/reserve_stock.lua
+++ scripts/redis/reserve_stock.lua
@@ -1,16 +1,17 @@
-local stockKey = KEYS[1]
-local amount = tonumber(ARGV[1])
+var reserveStockLua = `
+    local stockKey = KEYS[1]
+    local amount = tonumber(ARGV[1])
 
-local current = redis.call('get', stockKey)
-if not current then
-    return -1 -- Ключ не найден в Redis
-end
+    local current = redis.call('get', stockKey)
+    if not current then
+        return -1
+    end
 
-local currentStock = tonumber(current)
-if currentStock < amount then
-    return -2 -- Недостаточно билетов (Sold Out)
-end
+    local currentStock = tonumber(current)
+    if currentStock < amount then
+        return -2
+    end
 
--- decrby автоматически возвращает НОВЫЙ остаток после списания (например, 99, 50, или 0)
-local remaining = redis.call('decrby', stockKey, amount)
-return remaining
+    local remaining = redis.call('decrby', stockKey, amount)
+    return remaining
+`
